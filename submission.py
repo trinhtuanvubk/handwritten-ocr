7d90989257d21bd001d44e6f2d89cfa136cc5f6f
trinhtuanvubk <vutuantrinh2000@gmail.com>
2023-11-18 09:50:25 +0700

    update

--- submission.py
+++ submission.py
@@ -6,47 +6,33 @@ from utils import get_args
 import time
 import glob
 import csv
+from pyctcdecode import build_ctcdecoder
+import multiprocessing
 
-beam_decoder = nnet.BeamCTCDecoder(nnet.vi_dict, lm_path='nnet/ngram/address_l.arpa',
-                                 alpha=0, beta=0,
-                                 cutoff_top_n=40, cutoff_prob=1.0,
-                                 beam_width=5, num_processes=16,
-                                 blank_index=0)
+from utils.preprocess import detect_text_lines
 
-def submission_one_sample(args):
-    imgC, imgH, imgW = (3,48,720)
-    max_wh_ratio = imgW / imgH
-    model = nnet.get_models(args)
-    ckpt_path = "/home/sangdt/research/voice/svtr-pytorch/ckpt/SVTR_kalapa2310/checkpoints/SVTR.ckpt"
-    checkpoint = torch.load(ckpt_path, map_location=args.device)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    model = model.to(args.device)
-    model.eval()
-    postprocess = nnet.get_postprocess(args)
+# prepare decoder and decode logits via shallow fusion
+hotwords = ["Đg", "Ng", "ng", "Tdp", "TDP"]
+vi_dict = ['', 'a', 'A', 'à', 'À', 'ả', 'Ả', 'ã', 'Ã', 'á', 'Á', 'ạ', 'Ạ', 'ă', 'Ă', 'ằ', 'Ằ', 'ẳ', 'Ẳ', 'ẵ', 'Ẵ', 'ắ', 'Ắ', 'ặ', 'Ặ', 'â', 'Â', 'ầ', 'Ầ', 'ẩ', 'Ẩ', 'ẫ', 'Ẫ', 'ấ', 'Ấ', 'ậ', 'Ậ', 'b', 'B', 'c', 'C', 'd', 'D', 'đ', 'Đ', 'e', 'E', 'è', 'È', 'ẻ', 'Ẻ', 'ẽ', 'Ẽ', 'é', 'É', 'ẹ', 'Ẹ', 'ê', 'Ê', 'ề', 'Ề', 'ể', 'Ể', 'ễ', 'Ễ', 'ế', 'Ế', 'ệ', 'Ệ', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'ì', 'Ì', 'ỉ', 'Ỉ', 'ĩ', 'Ĩ', 'í', 'Í', 'ị', 'Ị', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'ò', 'Ò', 'ỏ', 'Ỏ', 'õ', 'Õ', 'ó', 'Ó', 'ọ', 'Ọ', 'ô', 'Ô', 'ồ', 'Ồ', 'ổ', 'Ổ', 'ỗ', 'Ỗ', 'ố', 'Ố', 'ộ', 'Ộ', 'ơ', 'Ơ', 'ờ', 'Ờ', 'ở', 'Ở', 'ỡ', 'Ỡ', 'ớ', 'Ớ', 'ợ', 'Ợ', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'ù', 'Ù', 'ủ', 'Ủ', 'ũ', 'Ũ', 'ú', 'Ú', 'ụ', 'Ụ', 'ư', 'Ư', 'ừ', 'Ừ', 'ử', 'Ử', 'ữ', 'Ữ', 'ứ', 'Ứ', 'ự', 'Ự', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'ỳ', 'Ỳ', 'ỷ', 'Ỷ', 'ỹ', 'Ỹ', 'ý', 'Ý', 'ỵ', 'Ỵ', 'z', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', "'", "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ' ', ' ']
 
-    start = time.time()
-    image = cv2.imread(args.image_test_path)
-    h, w = image.shape[0:2]
-    wh_ratio = w * 1.0 / h
-    max_wh_ratio = max(max_wh_ratio, wh_ratio)
+decoder = build_ctcdecoder(
+    vi_dict,
+    kenlm_model_path='ckpt/ngram/address_fix_811.bin',  # either .arpa or .bin file
+    # kenlm_model_path=None,
+    alpha=0.3,  # tuned on a val set
+    beta=2.0,  # tuned on a val set
+    # hotwords = hotwords,
+    # hotword_weight=5.0,
+)
 
-    norm_img = resize_norm_img(image, max_wh_ratio)
-
-    norm_img = torch.tensor(np.expand_dims(norm_img, axis=0)).to(args.device)
-
-    print(f"normshape: {norm_img.shape}")
-
-    output = model(norm_img)
-    output = output[0]
-    postprocessed_output = postprocess(output.cpu().detach().numpy())
-    print(postprocessed_output)
-    end = time.time()-start
-    print(f"time: {end}")
-    return postprocessed_output
-
+def check_num(word:str):
+    for i in word:
+        if i.isdigit():
+            return True
+    return False
 
 def submission(args, use_lm=True):
-    with open('/home/sangdt/research/voice/svtr-pytorch/data/OCR/2510_ngram.csv', 'a+') as f:
+    with open('./1611_0811.csv', 'a+') as f:
         writer = csv.writer(f,  delimiter=',')
         writer.writerow(["id", "answer"])
 
@@ -55,21 +41,21 @@ def submission(args, use_lm=True):
         model = nnet.get_models(args)
         model = model.to(args.device)
         # ckpt_path = "./ckpt/SVTR_kalapa2110/checkpoints/SVTR.ckpt"
-        ckpt_path = "./ckpt/best_2510/kala_lmdb_fix_aug_2410/checkpoints/SVTR.ckpt"
+        # ckpt_path = "./ckpt/SVTR_kalapa_611/checkpoints/SVTR.ckpt"
+        # ckpt_path = "./ckpt/SVTR_111123/SVTR_111123.ckpt"
+        ckpt_path = "./ckpt/SVTR_711_Son/SVTR_0811_0.92.ckpt"
         checkpoint = torch.load(ckpt_path, map_location=args.device)
         model.load_state_dict(checkpoint['model_state_dict'])
-        model.eval()
         model.eval()
         postprocess = nnet.get_postprocess(args)
 
         start = time.time()
 
         norm_img_batch = []
-        all_folder = os.listdir("./data/OCR/public_test/images/")
-        
 
         # Get a list of all subfolders
-        subfolders = glob.glob("./data/OCR/public_test/images/*")
+        subfolders = glob.glob("./data/public_test/images/*")
+        # subfolders = glob.glob("./data/kalapa_fixed_raw/train/images_note")
 
         # Get a list of all images in all subfolders
         image_path = []
@@ -77,13 +63,23 @@ def submission(args, use_lm=True):
         for subfolder in subfolders:
             image_path += glob.glob(subfolder + "/*.*")
         for i in image_path:
+            print(i)
             image = cv2.imread(i)
-            images.append({'image_name': "/".join(i.rsplit("/", 2)[-2:]), 'image': image})
+            image_name =  "/".join(i.rsplit("/", 2)[-2:])
+            image = detect_text_lines(image)
+            if image is None:
+                writer.writerow([image_name, ""])
+                continue
+            os.makedirs("./test_detect_line", exist_ok=True)
+            cv2.imwrite(os.path.join("./test_detect_line", i.split("/", 4)[-1].replace("/","_")), image)
+            images.append({'image_name': image_name, 'image': image})
+
+        
 
         # print(images)
         img_num = len(images)
         idx = 0
-        batch_num = 6
+        batch_num = 1
         for beg_img_no in range(0, img_num, batch_num):
             end_img_no = min(img_num, beg_img_no + batch_num)
             norm_img_batch = []
@@ -110,10 +106,15 @@ def submission(args, use_lm=True):
 
             output = model(norm_img_torch)[0]
             print(output.shape)
-            if use_lm:
+            print(output)
+            if args.decode_type == use_lm:
                 try:
-                    beam_output, _ = beam_decoder.decode(output)
-                    postprocessed_output = [output[0].strip() for output in beam_output]
+                    with multiprocessing.get_context("fork").Pool() as pool:
+                        postprocessed_output = decoder.decode_batch(pool, output.cpu().detach().numpy())
+                                                                    # beam_prune_logp=-15,
+                                                                    # token_min_logp=-7)
+                    # postprocessed_output = decoder.decode(output[0].cpu().detach().numpy())
+                    postprocessed_output = [i.replace("  "," ").replace("uỵ", "ụy") for i in postprocessed_output]
                     print(postprocessed_output)
 
                     for i, j in zip(name_batch, postprocessed_output):
@@ -122,10 +123,10 @@ def submission(args, use_lm=True):
                         #     j = [""]
                         writer.writerow([i, j])
                 except:
-                    print(beam_output)
+                    print("hihi")
 
             # output = [i[0] for i in output]
-            else:
+            elif args.decode_type=="nolm":
                 postprocessed_output = postprocess(output.cpu().detach().numpy())
             
                 print(postprocessed_output)
@@ -133,12 +134,49 @@ def submission(args, use_lm=True):
                 for i, j in zip(name_batch, postprocessed_output):
                     print(i)
                     writer.writerow([i, j[0]])
+            elif args.decode_type=="both":
+                batch_last_output = []
+                # do lm first
+                with multiprocessing.get_context("fork").Pool() as pool:
+                    batch_lm_output = decoder.decode_batch(pool, output.cpu().detach().numpy(),
+                                                                    beam_prune_logp=-15,
+                                                                    token_min_logp=-7)
+                    # postprocessed_output = decoder.decode(output[0].cpu().detach().numpy())
+                    batch_lm_output = [i.replace("  "," ").replace("uỵ", "ụy") for i in batch_lm_output]
 
+                #  do normal
+                batch_nolm_output = postprocess(output.cpu().detach().numpy())
+                batch_nolm_output = [i[0] for i in batch_nolm_output]
+
+                # print(batch_nolm_output)
+                for lm_output, nolm_output in zip(batch_lm_output, batch_nolm_output):
+
+                    if len(lm_output.split(" ")) > len(nolm_output.split(" ")):
+                        last_output = lm_output
+                    elif len(lm_output.split(" ")) < len(nolm_output.split(" ")):
+                        last_output = nolm_output
+                    else:
+                        last_output = lm_output
+                        for lm_word, nolm_word in zip(lm_output.split(" "), nolm_output.split(" ")):
+                            if check_num(lm_word):
+                                last_output.replace(lm_word, nolm_word)
+                    batch_last_output.append(last_output)
+
+                for i, j in zip(name_batch, batch_last_output):
+                    print(i)
+                    writer.writerow([i, j])
+
+            
 
     # return postprocessed_output
 
-
+    
+    
 
 if __name__=="__main__":
     args = get_args()
-    submission(args, use_lm=True)
+    args.device = torch.device("cpu")
+    args.decode_type = "both"
+    submission(args, use_lm=False)
+
+
