8e19dba0f24ec00de2e3010d3a15b4cc10325b13
TuanVuTrinh <vutuantrinh2000@gmail.com>
2024-02-27 22:06:15 +0700

    remove unused modules

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -1,12 +1,13 @@
-import numpy as np
+
 import os
-from torch.utils.data import Dataset
 import lmdb
 import cv2
-import string
-import six
 from PIL import Image
 import yaml
+
+import numpy as np
+from torch.utils.data import Dataset
+
 from .data.imaug import transform, create_operators
 
 def load_config(file_path):
@@ -144,91 +145,3 @@ class LMDBDataSet(Dataset):
 
     def __len__(self):
         return self.data_idx_order_list.shape[0]
-
-
-'''
-class LMDBDataSet(Dataset):
-    def __init__(self, args, mode='train'):
-        super(LMDBDataSet, self).__init__()
-        self.batch_size = args.batch_size
-        self.data_dir = os.path.join(args.lmdb_data_path, mode)
-        self.do_shuffle = args.shuffle
-
-        self.lmdb_sets = self.load_hierarchical_lmdb_dataset(data_dir)
-        self.data_idx_order_list = self.dataset_traversal()
-        if self.do_shuffle:
-            np.random.shuffle(self.data_idx_order_list)
-
-    def load_hierarchical_lmdb_dataset(self):
-        lmdb_sets = {}
-        dataset_idx = 0
-        for dirpath, dirnames, filenames in os.walk(self.data_dir + '/'):
-            if not dirnames:
-                env = lmdb.open(
-                    dirpath,
-                    max_readers=32,
-                    readonly=True,
-                    lock=False,
-                    readahead=False,
-                    meminit=False)
-                txn = env.begin(write=False)
-                num_samples = int(txn.get('num-samples'.encode()))
-                lmdb_sets[dataset_idx] = {"dirpath":dirpath, "env":env, \
-                    "txn":txn, "num_samples":num_samples}
-                dataset_idx += 1
-        return lmdb_sets
-
-    def dataset_traversal(self):
-        lmdb_num = len(self.lmdb_sets)
-        total_sample_num = 0
-        for lno in range(lmdb_num):
-            total_sample_num += self.lmdb_sets[lno]['num_samples']
-        data_idx_order_list = np.zeros((total_sample_num, 2))
-        beg_idx = 0
-        for lno in range(lmdb_num):
-            tmp_sample_num = self.lmdb_sets[lno]['num_samples']
-            end_idx = beg_idx + tmp_sample_num
-            data_idx_order_list[beg_idx:end_idx, 0] = lno
-            data_idx_order_list[beg_idx:end_idx, 1] \
-                = list(range(tmp_sample_num))
-            data_idx_order_list[beg_idx:end_idx, 1] += 1
-            beg_idx = beg_idx + tmp_sample_num
-        return data_idx_order_list
-
-    def get_img_data(self, value):
-        """get_img_data"""
-        if not value:
-            return None
-        imgdata = np.frombuffer(value, dtype='uint8')
-        if imgdata is None:
-            return None
-        imgori = cv2.imdecode(imgdata, 1)
-        if imgori is None:
-            return None
-        return imgori
-
-    def get_lmdb_sample_info(self, txn, index):
-        label_key = 'label-%09d'.encode() % index
-        label = txn.get(label_key)
-        if label is None:
-            return None
-        label = label.decode('utf-8')
-        img_key = 'image-%09d'.encode() % index
-        imgbuf = txn.get(img_key)
-        return imgbuf, label
-
-    def __getitem__(self, idx):
-        lmdb_idx, file_idx = self.data_idx_order_list[idx]
-        lmdb_idx = int(lmdb_idx)
-        file_idx = int(file_idx)
-        sample_info = self.get_lmdb_sample_info(self.lmdb_sets[lmdb_idx]['txn'],
-                                                file_idx)
-        if sample_info is None:
-            return self.__getitem__(np.random.randint(self.__len__()))
-        img, label = sample_info
-        data = {'image': img, 'label': label}
-        return data
-
-    def __len__(self):
-        return self.data_idx_order_list.shape[0]
-'''
